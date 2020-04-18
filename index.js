5bc6ba1e7b82e576e8e252454e87cc3c02852b5b
aahmed019 <42854126+aahmed019@users.noreply.github.com>
2020-04-17 22:10:41 -0400

    removed some files

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = 3000;
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'topline99',
+    password: 'password',
     database: 'twizzle',
     multipleStatements: true,
     // A `connectionLimit` of 4 works nicely on my machine.  YMMV.
