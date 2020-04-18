05c98da194cc26dc3d562e91faec7e14ddb9b619
aahmed019 <42854126+aahmed019@users.noreply.github.com>
2020-04-18 13:10:50 -0400

    populated search bar

--- indexfc.js
+++ indexfc.js
@@ -9,7 +9,7 @@ const port = 3000;
 const pool = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
-    password : 'topline99',
+    password : 'password',
     database : 'fanschoice',
     multipleStatements : true,
     connectionLimit : 4
