468490687812bae8509d00a5c6433e6ce6080c12
mdrahman2020 <63753300+mdrahman2020@users.noreply.github.com>
2020-04-19 12:28:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
@@ -5,71 +6,36 @@ const hb = require('express-handlebars');
 const app = express();
 const mysql = require('mysql');
 
-
-/* Configuration */
-// You can configure the app by changing the values of the global variables in this block.
-// A more sophisticated way of dealing with configuration options is command line arguments,
-// environment variables, and configuration files.
-
-// The server will listen on port 3000 on all interfaces.
 const port = 3000;
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'twizzle',
-    multipleStatements: true,
-    // A `connectionLimit` of 4 works nicely on my machine.  YMMV.
-    connectionLimit: 4
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Rayan1234',
+    database : 'fanschoice',
+    multipleStatements : true,
+    connectionLimit : 4
 });
 
-// The server will serve static content (javascript, css, images, etc.) out of the `static` directory.
 app.use(express.static('static'));
-// Cookies can (and should!) be encrypted,
-// but this functionality is only available in express if being served from an HTTPS connection.
-// In a production environment, the following line would be uncommented,
-// but for our purposes unencrypted cookies are fine.
-// app.use(session({ secret: 'csc336', cookie: { secure: true } }));
-app.use(session({
-    secret: 'csc336',
-    resave: true,
-    saveUninitialized: true
-}));
-// (You can "hack" the website by editing your session cookie from the web-developer toolbar and setting `user_id` to a different user id.)
-
-// Parse urlencoded data out of form submissions
+app.use(session({ secret: 'csc336' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded());
-
-// Register `/views/layouts/main.handlebars` with handlebars.
-// More information about this library can be found at:
-// https://github.com/ericf/express-handlebars
-app.engine('handlebars', hb({ defaultLayout: 'main' }));
+app.engine('handlebars', hb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-/* End of configuration */
-
-const query_users = 'SELECT name FROM users ORDER BY name;';
-const query_followers = 'SELECT * FROM follows JOIN users ON source = id WHERE target = get_user_id_from_name(?) ORDER BY updated, name;';
-const query_followees = 'SELECT * FROM follows JOIN users ON target = id WHERE source = get_user_id_from_name(?) ORDER BY updated, name;';
-
-// The `query_twizzles` query has essentially been replaced by the `stream` view.
-// const query_twizzles = 'SELECT * FROM twizzles JOIN follows ON user_id = followee NATURAL JOIN users WHERE follower = ? OR id = ? ORDER BY posted';
-const query_stream = 'SELECT * FROM stream WHERE stream_id = get_user_id_from_name(?) ORDER BY posted DESC;';
-
-const query_create_user = 'INSERT INTO users (name, displayname) VALUES (?, ?);';
-const query_follow = 'CALL FollowUser(?, get_user_id_from_name(?));';
-const query_unfollow = 'CALL UnfollowUser(?, get_user_id_from_name(?));';
-const query_post_twizzle = 'INSERT INTO twizzles (user_id, content) VALUES (?, ?);';
-const query_user_id_from_name = 'SELECT id FROM users WHERE name = ?;';
-
-/**
- * Checks to see if a user is authenticated (logged in).
- *
- * The `req.session` object contains the contents of the session cookies.
- * If the user is logged in, then they will have a cookie with `user_id` set to their user id.
- * If the user does not have such a cookie, redirect them to the login page.
- * Otherwise, proceed.
- */
+//query list
+const query_users = 'SELECT userName FROM user ORDER BY userName;';
+const query_stream = 'SELECT * FROM allreviews ORDER BY posted DESC;';
+const query_create_user = 'INSERT INTO user (userName, email) VALUES (?, ?);';
+const query_user_id_from_name = 'SELECT id FROM user WHERE userName = ?;';
+const query_emails = 'SELECT email FROM user ORDER BY email;';
+const query_artist = 'SELECT name from artist;' 
+const query_artist_name = 'SELECT name from artist WHERE name = ?;' 
+const query_post_revA = 'INSERT INTO reviewAlbum (name, content) VALUES (?, ?);'; //
+const query_albums = 'select album.name from artist,album where album.artistid =artist.id and artist.name = ?;';
+const query_song = 'SELECT single.name FROM artist,single WHERE single.artistid = artist.id and artist.name= ?;';
+const query_topsingles = 'select name from sumlikessingle ORDER BY TOTAL DESC;';
+const query_topalbums = 'select name from sumlikesalbum ORDER BY TOTAL DESC;';
+ 
 function checkAuth(req, res, next) {
     if (!req.session.user_id) {
         res.redirect(302, '/login');
@@ -78,22 +44,11 @@ function checkAuth(req, res, next) {
     }
 }
 
-/**
- * Middleware that logs a request.  This might save you several hours of debugging.
- *
- * Details about the Express Request object can be found at:
- * https://expressjs.com/en/api.html#req
- */
 function debuglog(req, res, next) {
     console.debug(`${req.method} -- ${req.path} (params: ${JSON.stringify(req.params)}) [body: ${JSON.stringify(req.body)}]`);
     next();
 }
 
-/**
- * Middleware that allows us to use a database connection from inside of a route.
- * More information about express middleware can be found at:
- * https://expressjs.com/en/guide/using-middleware.html
- */
 function db(req, res, next) {
     pool.getConnection((error, connection) => {
         try {
@@ -105,7 +60,6 @@ function db(req, res, next) {
                 next();
             }
         } finally {
-            // We are done with the connection, so we release it back to the pool to allow someone else to use it.
             connection.release();
         }
     });
@@ -125,11 +79,6 @@ function query_chain(connection, queries) {
     })));
 }
 
-/**
- * Route for a GET request to the login page.
- *
- * Renders the login page template from `views/login.handlebars` or an error page if the query fails.
- */
 app.get('/login', debuglog, db, function (req, res) {
     // The `query_users` variable holds a query which returns a list of all of the site's users.
     req.connection.query(query_users, (error, results, fields) => {
@@ -143,6 +92,7 @@ app.get('/login', debuglog, db, function (req, res) {
     });
 });
 
+
 app.post('/login', debuglog, db, function (req, res) {
     req.connection.query(query_user_id_from_name, [req.body.user], (error, result) => {
         if (error) {
@@ -153,6 +103,7 @@ app.post('/login', debuglog, db, function (req, res) {
                 req.session.user_id = result[0].id;
                 req.session.user_name = req.body.user;
                 res.redirect(`/user/${req.session.user_name}`);
+
             } else {
                 console.error('Error logging in, unexpected result set.');
             }
@@ -160,9 +111,19 @@ app.post('/login', debuglog, db, function (req, res) {
     });
 });
 
-app.get('/songs', debuglog, db, function (req, res) {
-    res.render('songs');
+// for top songs
 
+app.get('/songs', debuglog, db, function (req, res) {
+    // The `query_users` variable holds a query which returns a list of all of the site's users.
+    req.connection.query(query_topsingles, (error, singlenames, fields) => {
+        if (error) {
+            console.error('Error executing `query_users`.', error);
+            res.render('error', { error });
+        } else {
+            console.log(singlenames);
+            res.render('songs', {singlenames});
+        }
+    });
 });
 
 app.post('/songs', debuglog, db, function (req, res) {
@@ -183,66 +144,34 @@ app.post('/stream', debuglog, db, function (req, res) {
 });
 
 app.get('/profile', debuglog, db, function (req, res) {
+
     res.render('profile');
 
 });
 
 app.post('/profile', debuglog, db, function (req, res) {
-
+    console.degbug(result[0])
     res.redirect('/profile');
 
 });
-
-
-app.get('/search', debuglog, db, function (req, res) {
-    // The `query_users` variable holds a query which returns a list of all of the site's users.
-    req.connection.query(query_users, (error, results, fields) => {
-        if (error) {
-            console.error('Error executing `query_users`.', error);
-            res.render('error', { error });
-        } else {
-            // We pass the list of users to the `login` template to populate the drop down.
-            res.render('search', { results });
-        }
-    });
-});
-
- app.post('/search', debuglog, db, function (req, res) {
-    res.redirect('/search');
-});
-
 
 app.post('/logout', debuglog, function (req, res) {
     delete req.session.user_id;
     res.redirect('/login');
 });
 
-
-app.get('/', debuglog, db, checkAuth, function (req, res) {
+app.get('/', debuglog, db, checkAuth, function(req, res) {
     res.redirect(`/user/${req.session.user_name}`);
 });
 
-
-
-app.get('/user/:name', debuglog, db, function (req, res) {
-    // Is the user visiting thier own stream?
-    let is_own_stream = (req.params.name === req.session.user_name);
+app.get('/user/:name', debuglog, db, function(req, res) { //** 
     console.log(req.params.name, req.session.user_name, req.params.name === req.session.user_name);
     query_chain(req.connection, [
         [query_stream, [req.params.name]],
-        [query_followees, [req.params.name]],
-        [query_followers, [req.params.name]]
-    ]).then(([stream, followees, followers]) => {
-        let is_following = followers.find(e => { return e.name == req.session.user_name }) !== undefined;
-        let can_follow = !is_own_stream && !is_following;
-        let can_unfollow = is_following;
+    
+    ]).then(([reviewAlbum]) => {
         let result = {
-            stream,
-            followees,
-            followers,
-            can_follow,
-            can_unfollow,
-            is_own_stream,
+            reviewAlbum,  
             stream_name: req.params.name,
             user_name: req.session.user_name
         };
@@ -253,45 +182,63 @@ app.get('/user/:name', debuglog, db, function (req, res) {
     });
 });
 
-app.post('/user/:name/follow', debuglog, db, checkAuth, function (req, res) {
-    console.log(req.session.user_id, req.params.name);
-    req.connection.query(query_follow, [req.session.user_id, req.params.name], (error, result, fields) => {
-        if (error) {
-            console.error('Error following user:', error);
-            res.redirect(500, '/?error=unknown');
-            return;
-        }
-        res.redirect(302, `/user/${req.params.name}`);
+app.get('/search', debuglog, db, function (req, res) {
+    query_chain(req.connection, [
+        [query_artist, [req.params.name]],
+        [query_albums, [req.params.name]],
+        [query_song, [req.params.name]],
+        [query_emails, [req.params.name]]
+
+    ]).then(([artist,album,song,email]) => {
+        let result = {
+            artist,
+            album,
+            song,
+            email
+        };
+        res.render('search', result);
+    }).catch((error) => {
+        console.log(error);
+        res.render('error', { error });
     });
 });
 
-app.post('/user/:name/unfollow', debuglog, db, checkAuth, function (req, res) {
-    req.connection.query(query_unfollow, [req.session.user_id, req.params.name], (error, result, fields) => {
-        if (error) {
-            console.error('Error unfollowing user:', error);
-            res.redirect(500, '/?error=unknown');
-            return;
-        }
-        res.redirect(302, `/user/${req.params.name}`);
-    });
+ app.post('/search', debuglog, db, function (req, res) {
+    res.redirect('/search');
 });
 
-app.post('/post', debuglog, db, checkAuth, function (req, res) {
-    req.connection.query(query_post_twizzle, [req.session.user_id, req.body.twizzle], (error, stream, fields) => {
-        if (error) {
-            res.render('error', { error });
-        } else {
-            res.redirect(302, '/');
-        }
+app.post('/search/artist', debuglog, db, function(req, res) {
+    if (!req.body) {
+        res.redirect(500, '/search?error=unknown');
+    }
+    console.log(req.body.artist_name);
+    query_chain(req.connection, [
+        [query_artist, [req.body.artist_name]],
+        [query_artist_name, [req.body.artist_name]],
+        [query_albums, [req.body.artist_name]],
+        [query_song, [req.body.artist_name]],
+        [query_emails, [req.body.artist_name]]
+    ]).then(([artist,artists,album,song,email]) => {
+        let result = {
+            artist,
+            artists,
+            album,
+            song,
+            email
+        };
+        res.render('search', result);
+    }).catch((error) => {
+        console.log(error);
+        res.render('error', { error });
     });
-});
+    });
 
-app.post('/api/user', debuglog, db, function (req, res) {
+app.post('/api/user', debuglog, db, function(req, res) {
     if (!req.body) {
         res.redirect(500, '/login?error=unknown');
     }
 
-    req.connection.query(query_create_user, [req.body.user_name, req.body.display_name], (error, result, fields) => {
+    req.connection.query(query_create_user, [req.body.user_name, req.body.display_name], (error, result, fields) => { // works
         if (error) {
             res.redirect(500, '/login?error=unknown');
             return;
@@ -300,15 +247,18 @@ app.post('/api/user', debuglog, db, function (req, res) {
     });
 });
 
-app.get('/error', debuglog, db, function (req, res) {
+app.post('/post', debuglog, db, checkAuth, function(req, res) { // use this for review page
+    req.connection.query(query_post_revA, [req.session.user_name, req.body.twizzle], (error, stream, fields) => {
+        if (error) {
+            res.render('error', { error });
+        } else {
+            res.redirect(302, '/');
+        }
+    });
+}); //
+
+app.get('/error', debuglog, db, function(req, res) {
     res.render('error', { error: req.query.type === 'preview' ? 'Cannot preview.' : 'Unknown error.' });
 });
 
-
-
-
-
-
 app.listen(3000);
-
-
